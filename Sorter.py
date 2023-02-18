e7e6da4640e2b1c7bdd20c0b28ed864be6fe09fd
Rumanov Leonid <rumanovleonid@hotmail.com>
2023-02-19 00:11:03 +0300

    Debounce function changed

--- Sorter.py
+++ Sorter.py
@@ -1,8 +1,9 @@
-import cv2
-import datetime
 import os
-from pyzbar import pyzbar
+import datetime
+import time
 from glob import glob
+import cv2
+from pyzbar import pyzbar
 
 
 QRdata = []
@@ -14,35 +15,51 @@ def getQr(filename):
     qrcodes = pyzbar.decode(img)  # Создается список найденных кодов
     for qrcode in qrcodes:
         qrcodeData = qrcode.data.decode('utf-8')
-        #if qrcode.type == 'QRCODE' in qrcodeData:  # проверяем тип кода и проверяем вхождение строки 'fn='
+        # if qrcode.type == 'QRCODE' in qrcodeData:  # проверяем тип кода и проверяем вхождение строки 'fn='
         all_data.append(qrcodeData.split('&'))
     return all_data
 
-def LoadQRSample(dir_path = os.path.dirname(os.path.realpath(__file__)) + "\\QRsample\\"):
+
+def LoadQRSample(dir_path=os.path.dirname(os.path.realpath(__file__)) + "\\QRsample\\"):
     for filename in glob('QRsample/*.jpg'):  # Считываем тип файлов только jpg
         QRdata.append(getQr(filename))
-     
+
+
 def ScanQR():
     _, img = cap.read()
     data, bbox, _ = detector.detectAndDecode(img)
     if bbox is not None:
         if data:
-           return data
-    return 
+            return data
+    return
 
-def DebounceScanQR(): 
+
+""" def DebounceScanQR():
     while True:
         start = datetime.datetime.now().second
-        
-        dataStart = ScanQR()
-        dataStop = dataStart
 
-        delta = datetime.datetime.now().second - start      
-       
-        if delta < 1 and (dataStart == dataStop): continue
-        elif dataStart is None: continue 
+        dataStart = ScanQR()
+        # dataStop = dataStart
+
+        delta = datetime.datetime.now().second - start
+
+        if delta < 1 and (dataStart is not ScanQR):
+            continue
+        elif dataStart is None:
+            continue
         else:
-            return dataStart 
+            return dataStart """
+
+
+def DebounceScanQR():
+    while True:
+        scan = ScanQR()
+        time.sleep(1)
+        rescan = ScanQR()
+
+        if (scan == rescan) and (scan != None):
+            return scan
+
 
 LoadQRSample()
 
@@ -52,8 +69,8 @@ detector = cv2.QRCodeDetector()
 while True:
     data = DebounceScanQR()
     print("[+] QR Code detected, data:", data)
-    #cv2.imshow("img", img)    
+    # cv2.imshow("img", img)
     if cv2.waitKey(1) == ord("q"):
         break
 cap.release()
-#cv2.destroyAllWindows()
+# cv2.destroyAllWindows()
