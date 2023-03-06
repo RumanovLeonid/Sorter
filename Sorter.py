b0a4d0eab31a0d89a14d27338014531fe16060c6
Rumanov Leonid <rumanovleonid@hotmail.com>
2023-03-06 23:49:31 +0300

    Minimal working prototype.

--- Sorter.py
+++ Sorter.py
@@ -1,59 +1,36 @@
-import os
-import datetime
-import time
-from glob import glob
+"""Импорт функция задержки"""
+from time import sleep
 import cv2
-from pyzbar import pyzbar
-import keyboard
 
 SCAN_DELAY = 1
-QRdata = []
 
 
-def getQr(filename):
-    all_data = []
-    img = cv2.imread(filename)    # Считываем файл с изображением
-    qrcodes = pyzbar.decode(img)  # Создается список найденных кодов
-    for qrcode in qrcodes:
-        qrcodeData = qrcode.data.decode('utf-8')
-        all_data.append(qrcodeData.split('&'))
-    return all_data
-
-
-def LoadQRSample(dir_path=os.path.dirname(os.path.realpath(__file__)) + "\\QRsample\\"):
-    for filename in glob('QRsample/*.jpg'):
-        QRdata.append(getQr(filename))
-
-
-def ScanQR():
+def scan_qr():
+    """Сканирование QR кода"""
     _, img = cap.read()
-    data, bbox, _ = detector.detectAndDecode(img)
+    qr_code, bbox, _ = detector.detectAndDecode(img)
     if bbox is not None:
-        if data:
-            return data
-    return
+        if qr_code:
+            return qr_code
+    return None
 
 
-def DebounceScanQR():
+def debounce_scanqr():
+    """Исключение ложного срабатывания"""
     while True:
-        scan = ScanQR()
-        time.sleep(SCAN_DELAY)
-        rescan = ScanQR()
+        scan = scan_qr()
+        sleep(SCAN_DELAY)
+        rescan = scan_qr()
 
-        if (scan == rescan) and (scan != None):
+        if (scan == rescan) and (scan is not None):
             return scan
-
-
-LoadQRSample()
 
 
 cap = cv2.VideoCapture(0)
 detector = cv2.QRCodeDetector()
 
 while True:
-
-    data = DebounceScanQR()
-
+    data = debounce_scanqr()
     match data:
         case "Water":
             print("Water")
